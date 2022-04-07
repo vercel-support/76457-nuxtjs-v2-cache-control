2f6b4ca34d4303cbdeee47aaf209c6ec1e9524c1
William Li <email@williamli.dev>
2022-04-07 17:32:46 +0800

    looking into cache-control.js

--- server-middleware/cache-control.js
+++ server-middleware/cache-control.js
@@ -4,5 +4,7 @@ export default (req, res, next) => {
     's-maxage=86400, stale-while-revalidate'
   )
   res.setHeader('x-foo', 'bar')
+  res.setHeader('_test', 'Bend-Examine-Final-Famous-0')
+  console.log(res.getHeaders()) 
   next()
 }
