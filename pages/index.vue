c700b1ed07832538ee0e1de13e236aa98cc671ae
William Li <email@williamli.dev>
2022-04-07 16:56:53 +0800

    added pages and configured for Vercel SSR

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
+
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
